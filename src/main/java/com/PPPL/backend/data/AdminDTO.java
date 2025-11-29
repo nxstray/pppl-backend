f01666eb8f70b1e625a0a4c711d35442d380eb88
nxstray <afwanapriansyah@gmail.com>
2025-11-29 21:31:33 +0700

    done: prepare admin dto for fe

---
+++ src/main/java/com/PPPL/backend/data/AdminDTO.java
@@ -0,0 +1,20 @@
+package com.PPPL.backend.data;
+
+import com.PPPL.backend.model.AdminRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminDTO {
+    private Integer idAdmin;
+    private String username;
+    private String namaLengkap;
+    private String email;
+    private AdminRole role;
+    private Boolean isActive;
+    private java.util.Date lastLogin;
+    private java.util.Date createdAt;
+}
