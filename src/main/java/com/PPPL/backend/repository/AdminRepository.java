429ea96196c3bf2055b907c3127311c3c909e461
nxstray <afwanapriansyah@gmail.com>
2025-11-30 20:54:52 +0700

    done: repo for table admin

---
+++ src/main/java/com/PPPL/backend/repository/AdminRepository.java
@@ -0,0 +1,20 @@
+package com.PPPL.backend.repository;
+
+import com.PPPL.backend.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+    
+    Optional<Admin> findByUsername(String username);
+    
+    Optional<Admin> findByEmail(String email);
+    
+    boolean existsByUsername(String username);
+    
+    boolean existsByEmail(String email);
+    
+    Optional<Admin> findByUsernameAndIsActive(String username, Boolean isActive);
+}
