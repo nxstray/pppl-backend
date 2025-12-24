419a78d7368d5d8a40e94bf2ca25bffec4c38927
nxstray <afwanapriansyah@gmail.com>
2025-12-25 03:07:26 +0700

    fix: ft. serializable for message converter

--- src/main/java/com/PPPL/backend/data/NotificationEventDTO.java
+++ src/main/java/com/PPPL/backend/data/NotificationEventDTO.java
@@ -1,15 +1,16 @@
 package com.PPPL.backend.data;
 
 import lombok.Data;
+import java.io.Serializable;
 
 @Data
-public class NotificationEventDTO {
+public class NotificationEventDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String type;
     private String title;
     private String message;
     private String link;
-
-    // optional
     private String email;
     private boolean sendEmail;
     private boolean broadcastAdmin;
